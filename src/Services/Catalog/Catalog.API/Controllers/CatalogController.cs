e94717f5e811ba3f131e69a7d20b4b55544cdd90
Samvel Yeghikyan <samvel.yeghikyan@technamin.com>
2022-02-20 18:10:03 +0400

    .

---
+++ src/Services/Catalog/Catalog.API/Controllers/CatalogController.cs
@@ -0,0 +1,26 @@
+﻿using Catalog.API.Entities;
+using Catalog.API.Entities.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class CatalogController : ControllerBase
+    {
+        private readonly IProductRepository _repository;
+        private readonly ILogger<CatalogController> _logger;
+        public CatalogController(IProductRepository repository, ILogger<CatalogController> logger)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        }
+        [HttpGet]
+        public async Task<IEnumerable<Product>> GetProducts()
+        {
+            var products = await _repository.GetProducts();
+            return products;    
+        }
+    }
+}
