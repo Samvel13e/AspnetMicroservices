60cbcac7ce3bf01ec32433cbe296971a8e5aa910
Samvel Yeghikyan <samvel.yeghikyan@technamin.com>
2022-02-24 19:06:14 +0400

    .

--- src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
+++ src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -1,8 +1,26 @@
 ﻿using Discount.Grpc.Protos;
+using Discount.Grpc.Repositories;
+using Grpc.Core;
 
 namespace Discount.Grpc.Services
 {
-    public class DiscountService: DiscountProtoService.DiscountProtoServiceBase
+    public class DiscountService : DiscountProtoService.DiscountProtoServiceBase
     {
+        private readonly IDiscountReposotory _repository;
+        private readonly ILogger _logger;
+
+        public DiscountService(IDiscountReposotory repository, ILogger logger)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public override async Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+        {
+            var coupon = await _repository.GetDiscount(request.ProductName);
+            if (coupon is null)
+             throw new ArgumentNullException(nameof(coupon));   
+            //return await base.GetDiscount(request, context);
+        }
     }
 }
