6e8c1e5412b4d40647209fe1f610226b1493bc10
Samvel Yeghikyan <samvel.yeghikyan@technamin.com>
2022-03-09 09:23:13 +0400

    add ordering

---
+++ src/Services/Ordering/Ordering.Application/Contracts/Persistence/IOrderRepository.cs
@@ -0,0 +1,10 @@
+﻿using Ordering.Domain.Common.Entities;
+
+namespace Ordering.Application.Contracts.Persistence
+{
+    internal interface IOrderRepository : IAsyncRepository<Order>
+    {
+        Task<IEnumerable<Order>> GetOrderByUserName(string userName);
+
+    }
+}
