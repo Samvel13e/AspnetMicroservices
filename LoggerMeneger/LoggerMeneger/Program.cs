d1615feb7bd433c2300ef6f7269c84991b2e24f7
Samvel Yeghikyan <samvel.yeghikyan@technamin.com>
2022-04-22 17:45:48 +0400

    serilog

---
+++ LoggerMeneger/LoggerMeneger/Program.cs
@@ -0,0 +1,32 @@
+﻿using Serilog;
+using Serilog.Context;
+
+class MyClass
+{
+    static void Main()
+    {
+        string connStr = @"Data Source=.;Database=LogDB;user=Test;password=123";
+        string tblName = "Logs";
+        Log.Logger = new LoggerConfiguration()
+                    .MinimumLevel.Debug()
+                    .WriteTo.MSSqlServer(connStr, tblName)
+                    .CreateLogger();
+        Log.Information("infoi orinak");
+        try
+        {
+            int a = 23;
+            int b = 0;
+            Log.Debug($"Values are {a} and {b}");
+            int c = a/b;
+        }
+        catch (Exception ex)
+        {
+            LogContext.PushProperty("user", "sdffsf");
+            Log.Error(ex,"Some error!");
+        }
+        Log.CloseAndFlush();
+        Console.WriteLine("verj");
+        Console.ReadKey();
+    }
+
+}
